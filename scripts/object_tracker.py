ebafef66b71f15763306cd761b0138f0cc8fbcef
Gunnar Snorri Ragnarsson <gunnar.snorri.ragnarsson@gmail.com>
2015-12-13 12:01:18 +0100

    refactored object_tracker to class

--- scripts/object_tracker.py
+++ scripts/object_tracker.py
@@ -2,50 +2,47 @@
 import rospy
 from geometry_msgs.msg import PoseStamped, Twist
 
-pub = rospy.Publisher('object_tracker/pref_pos', Twist, queue_size=10)
-visp_pose = None
-pred_pose = None
 
-# Callback for VISP data subscription
+class ObjectTracker(object):
 
+    def __init__(self):
+        self.visp_pose = PoseStamped()
+        self.pred_pose = PoseStamped()
+        self.pub = rospy.Publisher(
+            'object_tracker/pref_pos',
+            Twist,
+            queue_size=10)
+        rospy.init_node('object_tracker', anonymous=True)
+        rospy.Subscriber(
+            "/visp_auto_tracker/object_position",
+            PoseStamped,
+            self.callback_visp)
+        rospy.Subscriber(
+            "/ardrone/predictedPose",
+            PoseStamped,
+            self.callback_pred)
 
-def callback_visp(pose):
-    global visp_pose
-    visp_pose = pose
+    def callback_visp(self, pose):
+        self.visp_pose = pose
 
+    def callback_pred(self, pose):
+        self.pred_pose = pose
 
-# Callback for predictedPose subscription
-def callback_pred(pose):
-    global pred_pose
-    pred_pose = pose
-
-
-def object_tracker():
-    rospy.init_node('object_tracker', anonymous=True)
-
-    rospy.Subscriber(
-        "/visp_auto_tracker/object_position",
-        PoseStamped,
-        callback_visp)
-    rospy.Subscriber("/ardrone/predictedPose", PoseStamped, callback_pred)
-
-    # create a twist message, fill in the details
-    twist = Twist()
-    twist.linear.x = 0
-    twist.linear.y = 0
-    twist.linear.z = 0
-    twist.angular.x = 0
-    twist.angular.y = 0
-    twist.angular.z = 0
-
-    # Publish the estimated waypoint on object_tracker/pref_pos
-    pub.publish(twist)
-    # spin() simply keeps python from exiting until this node is stopped
-    rospy.spin()
+    def run(self):
+        twist = Twist()
+        # Publish the estimated waypoint on object_tracker/pref_pos
+        r = rospy.Rate(100)  # in Hz
+        while not rospy.is_shutdown():
+            twist.linear.x = self.visp_pose.pose.position.x + \
+                self.pred_pose.pose.position.x
+            self.pub.publish(twist)
+            r.sleep()
+        # spin() simply keeps python from exiting until this node is stopped
 
 
 if __name__ == '__main__':
     try:
-        object_tracker()
+        tracker = ObjectTracker()
+        tracker.run()
     except rospy.ROSInterruptException:
         pass
