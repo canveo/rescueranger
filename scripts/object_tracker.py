7c0bdbc7a7d684c3ad7a3cd772e9beb5030117ed
Fawenah <tomas.nilsson.onsala@gmail.com>
2015-12-14 17:40:17 +0100

    Created development branch and small changes to the RR script

--- scripts/object_tracker.py
+++ scripts/object_tracker.py
@@ -6,21 +6,25 @@ from geometry_msgs.msg import PoseStamped, Twist
 class ObjectTracker(object):
 
     def __init__(self):
+        print 'Rescuranger initilizing'
         self.visp_pose = PoseStamped()
         self.pred_pose = PoseStamped()
         self.pub = rospy.Publisher(
             'object_tracker/pref_pos',
             Twist,
             queue_size=10)
+        print '...'
         rospy.init_node('object_tracker', anonymous=True)
         rospy.Subscriber(
             "/visp_auto_tracker/object_position",
             PoseStamped,
             self.callback_visp)
+        print '...'
         rospy.Subscriber(
             "/ardrone/predictedPose",
             PoseStamped,
             self.callback_pred)
+        print '...initilized\n'
 
     def callback_visp(self, pose):
         self.visp_pose = pose
@@ -29,14 +33,20 @@ class ObjectTracker(object):
         self.pred_pose = pose
 
     def run(self):
+        print 'Rescuranger Started\n Publishing to "/object_tracker/pref_pos"\n'
         twist = Twist()
         # Publish the estimated waypoint on object_tracker/pref_pos
         r = rospy.Rate(100)  # in Hz
         while not rospy.is_shutdown():
             twist.linear.x = self.visp_pose.pose.position.x + \
                 self.pred_pose.pose.position.x
+            twist.linear.y = self.visp_pose.pose.position.y + \
+                self.pred_pose.pose.position.y
+            twist.linear.z = self.visp_pose.pose.position.z + \
+                self.pred_pose.pose.position.z
             self.pub.publish(twist)
             r.sleep()
+        print '\n\nRescuranger is terminating.\n'
         # spin() simply keeps python from exiting until this node is stopped
 
 
