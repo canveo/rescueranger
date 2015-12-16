1442d8dfd4132ed9dd109ea24bd0cf40bb79cb91
Gunnar Snorri Ragnarsson <gunnar.snorri.ragnarsson@gmail.com>
2015-12-16 23:13:11 +0100

    keyboard controller, emergency landing

---
+++ scripts/keyboard_controller.py
@@ -0,0 +1,27 @@
+import rospy
+from keyboard.msg import Key
+
+
+class KeyboardController(object):
+
+    def __init__(self):
+        self.last_key = None
+        self.cmd_map = {
+            'start': 115,  # Start
+            'takeoff': 116,  # Takeoff
+            'land': 108,  # Land
+            'emergency': 101,  # Emergency
+        }
+        self.key_read = True
+        rospy.Subscriber('/keyboard/keydown', Key, self.callback_keyboard)
+
+    def callback_keyboard(self, data):
+        if data.code != self.last_key:
+            self.last_key = data.code
+            self.key_read = False
+
+    def get_key(self):
+        if not self.key_read:
+            return self.last_key
+        else:
+            return None
