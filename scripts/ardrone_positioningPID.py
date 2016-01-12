e0950e5a6bdcbf3ac5b6026dc2939fe0340e67ef
Fawenah <tomas.nilsson.onsala@gmail.com>
2016-01-12 15:15:23 +0100

    Fixed compile errors

--- scripts/ardrone_positioningPID.py
+++ scripts/ardrone_positioningPID.py
@@ -6,7 +6,7 @@ import time
 import sys
 from geometry_msgs.msg import Twist
 from tf2_msgs.msg import TFMessage
-from std_msgs.msg import Empty       	 # for land/takeoff/emergency
+from std_msgs.msg import Empty            # for land/takeoff/emergency
 from std_msgs.msg import Int8
 from ardrone_autonomy.msg import Navdata  # for receiving navdata feedback
 from keyboard_controller import KeyboardController
@@ -154,14 +154,14 @@ class ObjectTracker(object):
 
     def callback_ardrone_navdata(self, navdata):
         
-		#get state
+        #get state
         if self.ardrone_state != navdata.state:
             print("Recieved droneState: %d" % navdata.state)
             self.ardrone_state = navdata.state
 
-    	#get tilt
-    	self.ardrone_rotY = navdata.rotY*math.pi/180 #from degrees to radians
-    	self.ardrone_velY = navdata.vy/1000 #from mm/s to m/s
+        #get tilt
+        self.ardrone_rotY = navdata.rotY*math.pi/180 #from degrees to radians
+        self.ardrone_velY = navdata.vy/1000 #from mm/s to m/s
 
     def ardrone_send_takeoff(self):
         # Send a takeoff message to the ardrone driver
@@ -309,11 +309,11 @@ class ObjectTracker(object):
                     marker_size = 0.7
                     marker_fov_vertical = marker_size/real_distance #approximate 
                     max_allowed_vertical_marker_angle = (camera_fov_vertical - marker_fov_vertical)/2
-                    if abs(self.ardrone_rotY) < max_allowed_vertical_marker_angle
-						reference_vertical_marker_angle = -self.ardrone_rotY # negative sign because if we tilt up, marker should be down and vice versa
-					else
-						reference_vertical_marker_angle = math.copysign(max_allowed_vertical_marker_angle, -self.ardrone_rotY)
-					pidz_reference = self.goal_z_marker_frame + real_distance*math.sin(reference_vertical_marker_angle)
+                    if abs(self.ardrone_rotY) < max_allowed_vertical_marker_angle:
+                        reference_vertical_marker_angle = -self.ardrone_rotY # negative sign because if we tilt up, marker should be down and vice versa
+                    else:
+                        reference_vertical_marker_angle = math.copysign(max_allowed_vertical_marker_angle, -self.ardrone_rotY)
+                    pidz_reference = self.goal_z_marker_frame + real_distance*math.sin(reference_vertical_marker_angle)
                     pidyaw_reference = self.goal_yaw_marker_frame
                     
                     #update reference
